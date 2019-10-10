52186ec1a209404ca5cc4357d68b73e401af8628
Maciek Dom <maciej.rudnik2@gmail.com>
2019-10-10 22:07:49 +0200

    #02 Inicjalizacja servera
    Rozpoczęcie pracy nad serverem

---
+++ Software/iotserver/src/test/java/com/iotproject/server/iotserver/IotserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iotproject.server.iotserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IotserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
