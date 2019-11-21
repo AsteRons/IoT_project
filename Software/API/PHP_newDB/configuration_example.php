e2841117df3f1f65daf622a6258a44e542baa46f
MarcinJ7 <49407119+MarcinJ7@users.noreply.github.com>
2019-11-21 14:10:36 +0100

    #35 Dodanie przykładowego pliku konfiguracyjnego
    Wszystkie definicje należy oczywiście zmienić na własne...

---
+++ Software/API/PHP_newDB/configuration_example.php
@@ -0,0 +1,11 @@
+<?php
+    define("host_name","HOST");
+    define("db_username","USERNAME");
+    define("db_password","PASSword");
+    define("db_name","DBNAME");
+
+    define("dbToken_host_name","HOST");
+    define("dbToken_username","USERNAME");
+    define("dbToken_password","PASSword");
+    define("dbToken_name","DBNAME");
+?>
