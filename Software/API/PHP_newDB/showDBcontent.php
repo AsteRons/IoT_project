48709086ccbe100c617b6532265d64ce81bf43ef
MarcinJ7 <49407119+MarcinJ7@users.noreply.github.com>
2019-12-08 15:22:43 +0100

    #38 Utworzenie skryptu wyświetlającego dane (TOKEN)
    Skrypt został edytowany. Obecnie dane z bazy można wyświetlić wykorzystując skrypt jedynie, gdy posiadamy token. 
    Link uruchamiający wyświetlanie danych:
    https://iotprojectteam.000webhostapp.com/showDBcontentToken.php?token=...
    Jako parametr podajemy oczywiście "token".

--- Software/API/PHP_newDB/showDBcontent.php
+++ Software/API/PHP_newDB/showDBcontent.php
@@ -1,40 +1,105 @@
 <?php
-    $connection = @new mysqli('us-cdbr-iron-east-05.cleardb.net','b4d95917bcb107','705523ca','heroku_ac0c70f9d552a1b');
+    
+    $configPath = realpath(dirname(__FILE__));
+    error_reporting(0);
 
-    if(mysqli_connect_errno() !=0)
+    function myErrorHandler($errno, $errstr, $errfile, $errline) 
     {
-        echo("Error, unable to connect choosen database!");
+        echo("You have no data permission. Contact with MJ to buy tamporary access-MarcinJ7(GitHub)");
+    }
+    set_error_handler("myErrorHandler");
+
+    require_once($configPath."/config.php");
+
+    restore_error_handler();
+    
+    if($_GET['token']==true)
+    {
+        $token = rawurldecode($_GET['token']);
     }
     else
     {
-        echo("Connection succesful!");
+        echo("No parameters!");
+    }
 
-        $sql = "SELECT ID,deviceID,ntuValue,timestamp FROM sensordata";
+    $connection = @new mysqli(host_name,db_username,db_password,db_name);
 
+    if(mysqli_connect_errno() != 0)
+    {
+        echo("Error! Unable to connect this database!");
+    }
+    else
+    {
+        echo("Connection token DB succesful!");
+        echo("<br>");
+
+        $sql = "SELECT ID,token FROM tokenTable";
         $result = $connection->query($sql);
+        $tokenCorrect = false;
 
         if($result == true)
         {
-            echo("Query succesful."."<br>");
-
-            if($result->num_rows > 0)
+            while($row = $result->fetch_assoc())
             {
-                while($row = $result->fetch_assoc())
+                if($row['token']==$token)
                 {
-                    echo("ID:".$row["ID"]. " deviceID:".$row["deviceID"]." ntuValue:".$row["ntuValue"]." timestamp:".$row["timestamp"]);
-                    echo("<br>");
+                    $tokenCorrect = true;
+                    break;
                 }
-            }
-            else
-            {
-                echo("No rows in DB table!");
             }
         }
         else
         {
-            echo("Error while query asking! Try again or contact me (MJ)");
+            echo("Connection token DB succesfull, but query failed!");
         }
-
-        $connection->close();
     }
+
+    $connection->close();
+
+    if($tokenCorrect == true)
+    {
+        $connection = @new mysqli(dbToken_host_name,dbToken_username,dbToken_password,dbToken_name);
+
+        if(mysqli_connect_errno() != 0)
+        {
+            echo("Error! Can't connect with DB heroku");
+        }
+        else
+        {
+            echo("DB heroku connection succesful!");
+            
+            $sql = "SELECT ID,deviceID,ntuValue,timestamp FROM sensordata";
+
+            $result = $connection->query($sql);
+
+            if($result == true)
+            {
+                echo("Query succesful."."<br>");
+
+                if($result->num_rows > 0)
+                {
+                    while($row = $result->fetch_assoc())
+                    {
+                        echo("ID:".$row["ID"]. " deviceID:".$row["deviceID"]." ntuValue:".$row["ntuValue"]." timestamp:".$row["timestamp"]);
+                        echo("<br>");
+                    }
+                }
+                else
+                {
+                    echo("No rows in DB table!");
+                }
+            }
+            else
+            {
+                echo("Error while query asking! Try again or contact me (MJ)");
+            }
+        }
+    }
+    else
+    {
+        echo("Token is incorrect! For additional informations contact with support //MJ");
+    }
+
+    $connection->close();
+
 ?>
